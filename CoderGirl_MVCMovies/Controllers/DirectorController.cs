2ad170dee9a202b90e7e36b5388bae091030aa7d
Puja Grover <puja_grover@yahoo.com>
2019-05-19 18:48:18 -0500

    Puja Commit2

--- CoderGirl_MVCMovies/Controllers/DirectorController.cs
+++ CoderGirl_MVCMovies/Controllers/DirectorController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Data;
+using CoderGirl_MVCMovies.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoderGirl_MVCMovies.Controllers
 {
     public class DirectorController : Controller
     {
+        public static IDirectorRepository directorRepository = RepositoryFactory.GetDirectorRepository();
+
         public IActionResult Index()
         {
-            return View();
+            List<Director> directors = directorRepository.GetDirectors();
+            return View(directors);
         }
 
         public IActionResult Create()
